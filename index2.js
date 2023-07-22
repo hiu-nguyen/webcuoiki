f21a46e44156c997a0e7477446eaba70b19dc376
hiu-nguyen <hieuhanoi6@gmail.com>
2023-07-22 08:36:03 +0700

    1

---
+++ index2.js
@@ -0,0 +1,6 @@
+const A = document.querySelector("#a");
+const B = document.querySelector("#b");
+const C = document.querySelector("#c");
+const D = document.querySelector("#d");
+const choose = document.querySelector("#choose");
+
