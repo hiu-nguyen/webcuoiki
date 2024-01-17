c1c2564ad64174dac475fb2a46a77ea71dc14e99
hiu-nguyen <hieuhanoi6@gmail.com>
2024-01-18 00:16:13 +0700

    doiMK

--- index2.js
+++
@@ -1,6 +0,0 @@
-const A = document.querySelector("#a");
-const B = document.querySelector("#b");
-const C = document.querySelector("#c");
-const D = document.querySelector("#d");
-const choose = document.querySelector("#choose");
-
